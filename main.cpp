60221b2b80737f2c427c7abb6db0cfef821e7300
Popov Andrii <autoxarkov@gmail.com>
2020-09-10 01:04:09 +0300

    first commit

---
+++ main.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <windows.h>
+
+SYSTEMTIME t;
+
+DWORD WINAPI TreadTime(void* param) {
+
+    for (;; Sleep(1000)) {
+        GetSystemTime(&t);
+        std::cout << "Time: " << t.wHour << ":" << t.wMinute << ":" << t.wSecond << std::endl;
+    }
+
+    return 0;
+}
+
+int main()
+{
+    char* pValue;
+    size_t len;
+    errno_t err = _dupenv_s(&pValue, &len, "MYTIME");
+
+    int valueTime = 0;
+
+    if (!err) {
+        printf("MYTIME = %s\n", pValue);
+
+        valueTime = int(pValue); // * 1000
+    }
+
+    std::cout << valueTime << std::endl;
+
+    HANDLE thread = CreateThread(0, 0, TreadTime, 0, 0, 0);
+
+    Sleep(valueTime);
+
+    return 0;
+}
